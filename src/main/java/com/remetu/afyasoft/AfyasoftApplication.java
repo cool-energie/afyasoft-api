407b9835959101777a000f8a18f3338635685952
s.tayou <stevetayou@gmail.com>
2025-08-22 03:05:03 +0100

    Initial commit

---
+++ src/main/java/com/remetu/afyasoft/AfyasoftApplication.java
@@ -0,0 +1,13 @@
+package com.remetu.afyasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfyasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfyasoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remetu/afyasoft/AfyasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remetu.afyasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfyasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
